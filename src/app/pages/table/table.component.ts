b6c4c31e7375ee1c56c6b5e2265719b3e7933732
Matthew Lonis <mattlonis@gmail.com>
2026-03-10 02:13:11 -0400

    Add pagination, trackBy and sticky header
    Switch table dataSource to MatTableDataSource and wire up a MatPaginator (template paginator added with pageSize/options). Add a trackBy function and bind it on the table to improve rendering, and make the header row sticky. Import MatPaginatorModule and set the paginator in ngAfterViewInit via a @ViewChild reference; other table behavior remains the same.

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { PERIODIC_ELEMENTS, PeriodicElement } from '../../interfaces';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 /**
  * @title Table with expandable rows
@@ -11,13 +12,20 @@ import { PERIODIC_ELEMENTS, PeriodicElement } from '../../interfaces';
   selector: 'app-table',
   styleUrl: 'table.component.scss',
   templateUrl: 'table.component.html',
-  imports: [MatTableModule, MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, MatPaginatorModule, MatTableModule],
 })
-export class TableComponent {
-  dataSource = PERIODIC_ELEMENTS;
+export class TableComponent implements AfterViewInit {
+  dataSource = new MatTableDataSource<PeriodicElement>(PERIODIC_ELEMENTS);
   columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   expandedElement: PeriodicElement | null = null;
+  trackBy = (index: number, el: PeriodicElement) => el.position;
+
+  @ViewChild(MatPaginator) paginator?: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
   /** Checks whether an element is expanded. */
   isExpanded(element: PeriodicElement) {
